aaf4cf743b039c713dd3502359ba4080937a4dd6
Daniel <danieruone@gmail.com>
2022-03-14 20:51:45 -0500

    Add video player

--- src/components/videoStream/index.tsx
+++ src/components/videoStream/index.tsx
@@ -1,5 +1,6 @@
 // styles
 import styled from '@emotion/styled';
+import ReactPlayer from 'react-player';
 
 export const Container = styled.div`
   width: 100%;
@@ -14,21 +15,12 @@ export const Container = styled.div`
 export const VideoStream = () => {
   return (
     <Container>
-      <video
-        autoPlay
+      <ReactPlayer
         controls
-        x-webkit-airplay='true'
-        x5-video-player-fullscreen='true'
-        preload='auto'
-        webkit-playsinline
-        x5-video-player-typ='h5'
-      >
-        <source
-          src='http://167.99.227.213:3000/hls/test.m3u8'
-          type='application/x-mpegURL'
-        />
-        Your browser does not support the video tag.
-      </video>
+        width='100%'
+        height='100%'
+        url='http://167.99.227.213:3000/hls/test2.m3u8'
+      />
     </Container>
   );
 };
