4d42a28ecebb1f5fad25e0bf64b5ad3a542c234b
Daniel <danieruone@gmail.com>
2022-03-18 21:34:42 -0500

    Add login service

--- src/components/LoginForm/index.tsx
+++ src/components/LoginForm/index.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 // media
 import twitchLogo from 'assets/icons/twitch.png';
 
@@ -5,13 +7,29 @@ import twitchLogo from 'assets/icons/twitch.png';
 import { Typography } from '@mui/material';
 import { TextField } from '@mui/material';
 import Button from '@mui/material/Button';
+import CircularProgress from '@mui/material/CircularProgress';
 
 // styles
 import { Container, LogoContainer } from './styles';
 
+// services
+import { logIn } from 'services/Auth';
+
 export const LoginForm = () => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    setIsLoading(true);
+    const payload = { email: 'test@test.com', password: 'hola' };
+    logIn(payload)
+      .then((data) => console.log(data))
+      .catch((err) => console.log(err))
+      .finally(() => setIsLoading(false));
+  };
+
   return (
-    <Container>
+    <Container onSubmit={(e) => handleSubmit(e)}>
       <LogoContainer>
         <img src={twitchLogo} />
       </LogoContainer>
@@ -35,9 +53,13 @@ export const LoginForm = () => {
         // onChange={(e) => setMessage(e.target.value)}
       />
 
-      <Button variant='contained' color='warning'>
-        Send
-      </Button>
+      {isLoading ? (
+        <CircularProgress color='warning' />
+      ) : (
+        <Button variant='contained' color='warning' type='submit'>
+          Send
+        </Button>
+      )}
     </Container>
   );
 };
