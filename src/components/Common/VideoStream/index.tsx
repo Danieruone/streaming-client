f44b6469f066df9b9a8f67102ecb6fb6d2a3463a
Daniel <danieruone@gmail.com>
2022-03-23 18:55:28 -0500

    Add hls streaming events

--- src/components/Common/VideoStream/index.tsx
+++ src/components/Common/VideoStream/index.tsx
@@ -1,10 +1,10 @@
-import { FC, useState } from 'react';
+import { FC, useEffect, useRef, useState } from 'react';
 
 // UI
 import { Typography } from '@mui/material';
 
 // npm
-import ReactPlayer from 'react-player';
+import Hls from 'hls.js';
 
 // styles
 import { Container, OfflineBox } from './styles';
@@ -14,7 +14,40 @@ interface Props {
 }
 
 export const VideoStream: FC<Props> = ({ url }) => {
+  const videoRef: any = useRef(null);
+  const hls = new Hls({
+    liveSyncDurationCount: 3,
+    startPosition: -1,
+    liveDurationInfinity: true,
+  });
+
   const [isOnline, setIsOnline] = useState(false);
+
+  useEffect(() => {
+    if (videoRef.current) {
+      hls.attachMedia(videoRef.current);
+    }
+
+    hls.on(Hls.Events.MEDIA_ATTACHED, () => {
+      hls.loadSource(url);
+      hls.on(Hls.Events.MANIFEST_PARSED, () => {
+        hls.on(Hls.Events.BUFFER_APPENDED, () => {
+          setIsOnline(true);
+        });
+      });
+    });
+
+    hls.on(Hls.Events.ERROR, (event, data) => {
+      console.log(data);
+      if (
+        data.details === 'bufferStalledError' ||
+        data.type === 'networkError'
+      ) {
+        setIsOnline(false);
+        hls.destroy();
+      }
+    });
+  }, []);
 
   return (
     <Container>
@@ -28,7 +61,14 @@ export const VideoStream: FC<Props> = ({ url }) => {
           </div>
         </OfflineBox>
       )}
-      <ReactPlayer controls width='100%' height='100%' url={url} />
+      <video
+        ref={videoRef}
+        controls
+        autoPlay
+        muted
+        width='100%'
+        height='100%'
+      ></video>
     </Container>
   );
 };
