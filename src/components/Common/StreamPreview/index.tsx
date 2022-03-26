f4cfa38cff86c56f72c8760ce0dcdd093154bd1c
Daniel <danieruone@gmail.com>
2022-03-25 21:11:46 -0500

    Add streaming page items

---
+++ src/components/Common/StreamPreview/index.tsx
@@ -0,0 +1,28 @@
+// styles
+import { Avatar, Typography } from '@mui/material';
+import {
+  Container,
+  ImageContainer,
+  DirectBadge,
+  InfoContainer,
+} from './styles';
+
+export const StreamPreview = () => {
+  return (
+    <Container>
+      <ImageContainer>
+        <DirectBadge>EN DIRECTO</DirectBadge>
+        <img src='https://marketing4ecommerce.net/wp-content/uploads/2020/08/como-emitir-en-directo-en-Twitch-1280x720.jpg' />
+      </ImageContainer>
+      <InfoContainer>
+        <Avatar />
+        <div style={{ marginLeft: '1rem' }}>
+          <Typography variant='body1'>
+            <strong>Aquí jugando en stream, pasen!!</strong>
+          </Typography>
+          <Typography variant='body2'>Darulive</Typography>
+        </div>
+      </InfoContainer>
+    </Container>
+  );
+};
--- src/pages/OnlineStreams/index.tsx
+++ src/pages/OnlineStreams/index.tsx
@@ -1,6 +1,7 @@
 // components
 import { Navbar } from 'components/Common/Navbar';
 import { LeftBar } from 'components/Common/LeftBar';
+import { StreamPreview } from 'components/Common/StreamPreview';
 
 import styled from '@emotion/styled';
 
@@ -9,6 +10,14 @@ export const Container = styled.div`
   position: fixed;
   right: 0;
   left: 0;
+`;
+
+export const StreamsContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  padding: 3rem;
+  align-content: start;
+  width: 100%;
 `;
 
 export const OnlineStreams = () => {
@@ -21,7 +30,15 @@ export const OnlineStreams = () => {
         <div>
           <LeftBar />
         </div>
-        <div></div>
+        <StreamsContainer>
+          <StreamPreview />
+          <StreamPreview />
+          <StreamPreview />
+          <StreamPreview />
+          <StreamPreview />
+          <StreamPreview />
+          <StreamPreview />
+        </StreamsContainer>
       </Container>
     </div>
   );
