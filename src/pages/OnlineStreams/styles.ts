af643c96ec0f49e6328b1162e9f47acad0868b71
Daniel <danieruone@gmail.com>
2022-04-10 16:17:07 -0500

    Add empty streams copy

--- src/pages/OnlineStreams/styles.ts
+++ src/pages/OnlineStreams/styles.ts
@@ -14,3 +14,17 @@ export const StreamsContainer = styled.div`
   align-content: start;
   width: 100%;
 `;
+
+export const NoStreamsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;P
+  justify-content: center;
+  width: 100%;
+  margin-top: 15rem;
+  & h1 {
+    font-size: 2rem;
+    font-weight: 300;
+    color: gray;
+  }
+`;
